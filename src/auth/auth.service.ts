67f555a541931435005e281d8dc80af25d4d60be
DavidC <dav.capuano98@gmail.com>
2023-04-22 20:21:31 +0200

    adding JWT Strategy

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,22 @@
-import { ForbiddenException, Injectable } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
 import { User, Bookmark } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable({})
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+    private config: ConfigService,
+  ) {}
 
   async signup(dto: AuthDto) {
     // generate password hash
@@ -20,14 +29,14 @@ export class AuthService {
           hash,
         },
       });
-      // Dirty solution to not showing hash in returned user as response:
-      delete user.hash;
-      // return saved user
-      return user;
+
+      return this.signToken(user.id, user.email);
     } catch (error) {
       if (error instanceof PrismaClientKnownRequestError) {
         if (error.code === 'P2002') {
-          throw new ForbiddenException('Email already in use');
+          throw new ForbiddenException(
+            'Email already in use',
+          );
         }
       }
       // If is not a Prisma error, throw it
@@ -44,14 +53,39 @@ export class AuthService {
     });
 
     // If user not exist, throw an error
-    if (!user) throw new ForbiddenException('Incorrect email and/or password');
+    if (!user)
+      throw new ForbiddenException(
+        'Incorrect email and/or password',
+      );
 
     // Compare password. If passwords doesn't match, throw an error
-    const pwdMatches = await argon.verify(user.hash, dto.password);
-    if (!pwdMatches) throw new ForbiddenException('Incorrect email and/or password');
+    const pwdMatches = await argon.verify(
+      user.hash,
+      dto.password,
+    );
+    if (!pwdMatches)
+      throw new ForbiddenException(
+        'Incorrect email and/or password',
+      );
 
-    delete user.hash;
+    return this.signToken(user.id, user.email);
+  }
 
-    return user;
+  async signToken(
+    userId: number,
+    email: string,
+  ): Promise<{ access_token: string }> {
+    const payload = {
+      sub: userId,
+      email,
+    };
+    const secret = this.config.get('JWT_SECRET');
+
+    const token = await this.jwt.signAsync(payload, {
+      expiresIn: '15m',
+      secret,
+    });
+
+    return { access_token: token };
   }
 }
