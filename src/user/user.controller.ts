9fce58513f28bbc9c4062a7c29ec61d787051520
DavidC <dav.capuano98@gmail.com>
2023-04-26 21:55:24 +0200

    improvingn prettier & adding guards

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,18 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import {
+    Controller,
+    Get,
+    Req,
+    UseGuards,
+} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { Request } from 'express';
+import { JwtGuard } from 'src/auth/guard';
 
 @Controller('users')
 export class UserController {
-  @UseGuards(AuthGuard('jwt'))
-  @Get('me')
-  getMe() {
-    return 'user info';
-  }
+    @UseGuards(JwtGuard)
+    @Get('me')
+    getMe(@Req() req: Request) {
+        return req.user;
+    }
 }
