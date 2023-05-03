2ede9de1170fae52c6ca5fc296c83180a4e9e530
DavidC <dav.capuano98@gmail.com>
2023-05-03 22:25:32 +0200

    adding improvements & tests

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,6 @@
 import {
-  ForbiddenException,
-  Injectable,
+    ForbiddenException,
+    Injectable,
 } from '@nestjs/common';
 import { User, Bookmark } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
@@ -12,80 +12,83 @@ import { ConfigService } from '@nestjs/config';
 
 @Injectable({})
 export class AuthService {
-  constructor(
-    private prisma: PrismaService,
-    private jwt: JwtService,
-    private config: ConfigService,
-  ) {}
+    constructor(
+        private prisma: PrismaService,
+        private jwt: JwtService,
+        private config: ConfigService,
+    ) {}
 
-  async signup(dto: AuthDto) {
-    // generate password hash
-    const hash = await argon.hash(dto.password);
+    async signup(dto: AuthDto) {
+        // generate password hash
+        const hash = await argon.hash(dto.password);
 
-    try {
-      const user = await this.prisma.user.create({
-        data: {
-          email: dto.email,
-          hash,
-        },
-      });
+        try {
+            const user = await this.prisma.user.create({
+                data: {
+                    email: dto.email,
+                    hash,
+                },
+            });
 
-      return this.signToken(user.id, user.email);
-    } catch (error) {
-      if (error instanceof PrismaClientKnownRequestError) {
-        if (error.code === 'P2002') {
-          throw new ForbiddenException(
-            'Email already in use',
-          );
+            return this.signToken(user.id, user.email);
+        } catch (error) {
+            if (
+                error instanceof
+                PrismaClientKnownRequestError
+            ) {
+                if (error.code === 'P2002') {
+                    throw new ForbiddenException(
+                        'Email already in use',
+                    );
+                }
+            }
+            // If is not a Prisma error, throw it
+            throw error;
         }
-      }
-      // If is not a Prisma error, throw it
-      throw error;
     }
-  }
 
-  async signin(dto: AuthDto) {
-    // Find user by email
-    const user = await this.prisma.user.findUnique({
-      where: {
-        email: dto.email,
-      },
-    });
+    async signin(dto: AuthDto) {
+        // Find user by email
+        const user = await this.prisma.user.findUnique({
+            where: {
+                email: dto.email,
+            },
+        });
 
-    // If user not exist, throw an error
-    if (!user)
-      throw new ForbiddenException(
-        'Incorrect email and/or password',
-      );
+        // If user not exist, throw an error
+        if (!user)
+            throw new ForbiddenException(
+                'Incorrect email and/or password',
+            );
 
-    // Compare password. If passwords doesn't match, throw an error
-    const pwdMatches = await argon.verify(
-      user.hash,
-      dto.password,
-    );
-    if (!pwdMatches)
-      throw new ForbiddenException(
-        'Incorrect email and/or password',
-      );
+        // Compare password. If passwords doesn't match, throw an error
+        const pwdMatches = await argon.verify(
+            user.hash,
+            dto.password,
+        );
+        if (!pwdMatches)
+            throw new ForbiddenException(
+                'Incorrect email and/or password',
+            );
 
-    return this.signToken(user.id, user.email);
-  }
+        return this.signToken(user.id, user.email);
+    }
 
-  async signToken(
-    userId: number,
-    email: string,
-  ): Promise<{ access_token: string }> {
-    const payload = {
-      sub: userId,
-      email,
-    };
-    const secret = this.config.get('JWT_SECRET');
+    async signToken(
+        userId: number,
+        email: string,
+    ): Promise<{ access_token: string }> {
+        const payload = {
+            sub: userId,
+            email,
+        };
+        const secret = this.config.get('JWT_SECRET');
 
-    const token = await this.jwt.signAsync(payload, {
-      expiresIn: '15m',
-      secret,
-    });
+        const token = await this.jwt.signAsync(payload, {
+            expiresIn: '15m',
+            secret,
+        });
 
-    return { access_token: token };
-  }
+        return { access_token: token };
+    }
 }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -14,7 +14,11 @@ import { JwtGuard } from 'src/auth/guard';
 export class UserController {
     @UseGuards(JwtGuard)
     @Get('me')
-    getMe(@GetUser() user: User) {
+    getMe(
+        @GetUser() user: User,
+        @GetUser('email') email: string,
+    ) {
+        console.log({ email });
         return user;
     }
 }
