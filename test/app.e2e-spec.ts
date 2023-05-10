b90b4283a32c1628852a84b81f81cd0091c48233
DavidC <dav.capuano98@gmail.com>
2023-05-10 21:29:51 +0200

    setup for test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,25 +1,69 @@
 import { Test } from '@nestjs/testing';
 import { AppModule } from '../src/app.module';
 import {
-    INestApplication,
-    ValidationPipe,
+ INestApplication,
+ ValidationPipe,
 } from '@nestjs/common';
 
-describe('App e2e', () => {
-    let app: INestApplication;
-    beforeAll(async () => {
-        const moduleRef = await Test.createTestingModule({
-            imports: [AppModule],
-        }).compile();
-        app = moduleRef.createNestApplication();
-        app.useGlobalPipes(
-            new ValidationPipe({ whitelist: true }),
-        );
-        await app.init();
-    });
+import * as pactum from 'pactum';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from 'src/auth/dto';
+import e from 'express';
 
-    afterAll(async () => {
-        app.close();
-    });
-    it.todo('should pass');
+describe('App e2e', () => {
+ let app: INestApplication;
+ let prisma: PrismaService;
+
+ beforeAll(async () => {
+  const moduleRef = await Test.createTestingModule({
+   imports: [AppModule],
+  }).compile();
+
+  app = moduleRef.createNestApplication();
+  app.useGlobalPipes(
+   new ValidationPipe({ whitelist: true }),
+  );
+  await app.init();
+  await app.listen(3333);
+
+  prisma = app.get(PrismaService);
+  await prisma.cleanDb();
+ });
+
+ describe('Auth test', () => {
+  describe('Signup', () => {
+   it('Signup is successful', () => {
+    const requestBody: AuthDto = {
+     email: 'davidc@gmail.com',
+     password: 'Bye',
+    };
+    const expectedStatus = 201;
+    return pactum
+     .spec()
+     .post('http://localhost:3333/auth/signup')
+     .withBody(requestBody)
+     .expectStatus(expectedStatus);
+   });
+  });
+  describe('Signin', () => {});
+ });
+
+ describe('User test', () => {
+  describe('Get current user (me)', () => {});
+  describe('Get current user (me)', () => {});
+ });
+
+ describe('Bookmark test', () => {
+  describe('Create bookmark', () => {});
+  describe('Get bookmarks', () => {});
+  describe('Get bookmark by id', () => {});
+  describe('Edit bookmark', () => {});
+  describe('Delete bookmark', () => {});
+ });
+
+ it.todo('should pass');
+
+ afterAll(async () => {
+  app.close();
+ });
 });
