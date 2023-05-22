d4f229ed6ecd1ed2662c45a707dae36531b5f495
DavidC <dav.capuano98@gmail.com>
2023-05-22 19:20:57 +0200

    adding edit user feature and relative tests

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,5 @@
 import {
+    Body,
     Controller,
     Get,
     Patch,
@@ -8,10 +9,13 @@ import {
 import { User } from '@prisma/client';
 import { GetUser } from 'src/auth/decorator';
 import { JwtGuard } from 'src/auth/guard';
+import { EditUserDto } from './dto';
+import { UserService } from './user.service';
 
 @UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
+    constructor(private userService: UserService) {}
     @Get('me')
     getMe(
         @GetUser() user: User,
@@ -21,6 +25,11 @@ export class UserController {
         return user;
     }
 
-    @Patch('user')
-    editUser() {}
+    @Patch()
+    editUser(
+        @GetUser('id') userId: number,
+        @Body() dto: EditUserDto,
+    ) {
+        return this.userService.editUser(userId, dto);
+    }
 }
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,109 +1,131 @@
 import { Test } from '@nestjs/testing';
 import { AppModule } from '../src/app.module';
 import {
- INestApplication,
- ValidationPipe,
+    INestApplication,
+    ValidationPipe,
 } from '@nestjs/common';
 
 import * as pactum from 'pactum';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from 'src/auth/dto';
+import { EditUserDto } from 'src/user/dto';
 
 describe('App e2e', () => {
- let app: INestApplication;
- let prisma: PrismaService;
+    let app: INestApplication;
+    let prisma: PrismaService;
 
- beforeAll(async () => {
-  const moduleRef = await Test.createTestingModule({
-   imports: [AppModule],
-  }).compile();
+    beforeAll(async () => {
+        const moduleRef = await Test.createTestingModule({
+            imports: [AppModule],
+        }).compile();
 
-  app = moduleRef.createNestApplication();
-  app.useGlobalPipes(
-   new ValidationPipe({ whitelist: true }),
-  );
-  await app.init();
-  await app.listen(3333);
-  pactum.request.setBaseUrl('http://localhost:3333');
+        app = moduleRef.createNestApplication();
+        app.useGlobalPipes(
+            new ValidationPipe({ whitelist: true }),
+        );
+        await app.init();
+        await app.listen(3333);
+        pactum.request.setBaseUrl('http://localhost:3333');
 
-  prisma = app.get(PrismaService);
-  await prisma.cleanDb();
- });
+        prisma = app.get(PrismaService);
+        await prisma.cleanDb();
+    });
 
- describe('Auth test', () => {
-  const requestBody: AuthDto = {
-   email: 'davidc@gmail.com',
-   password: 'Bye',
-  };
-  describe('Signup', () => {
-   it('Signup is successful', () => {
-    const expectedStatus = 201;
-    return pactum
-     .spec()
-     .post('/auth/signup')
-     .withBody(requestBody)
-     .expectStatus(expectedStatus);
-   });
-   it('Signup is not successful and an error is thrown (empty username) ', () => {
-    const expectedStatus = 400;
-    return pactum
-     .spec()
-     .post('/auth/signup')
-     .withBody({ password: requestBody.password })
-     .expectStatus(expectedStatus);
-   });
-   it('Signup is not successful and an error is thrown (empty email) ', () => {
-    const expectedStatus = 400;
-    return pactum
-     .spec()
-     .post('/auth/signup')
-     .withBody({ password: requestBody.email })
-     .expectStatus(expectedStatus);
-   });
-   it('Signup is not successful and an error is thrown (empty body) ', () => {
-    const expectedStatus = 400;
-    return pactum
-     .spec()
-     .post('/auth/signup')
-     .withBody({})
-     .expectStatus(expectedStatus);
-   });
-  });
-  describe('Signin', () => {
-   it('should signin successfully', () => {
-    return pactum
-     .spec()
-     .post('/auth/signin')
-     .withBody(requestBody)
-     .expectStatus(200)
-     .stores('userAt', 'access_token');
-   });
-  });
- });
+    describe('Auth test', () => {
+        const requestBody: AuthDto = {
+            email: 'davidc@gmail.com',
+            password: 'Bye',
+        };
+        describe('Signup', () => {
+            it('Signup is successful', () => {
+                const expectedStatus = 201;
+                return pactum
+                    .spec()
+                    .post('/auth/signup')
+                    .withBody(requestBody)
+                    .expectStatus(expectedStatus);
+            });
+            it('Signup is not successful and an error is thrown (empty username) ', () => {
+                const expectedStatus = 400;
+                return pactum
+                    .spec()
+                    .post('/auth/signup')
+                    .withBody({
+                        password: requestBody.password,
+                    })
+                    .expectStatus(expectedStatus);
+            });
+            it('Signup is not successful and an error is thrown (empty email) ', () => {
+                const expectedStatus = 400;
+                return pactum
+                    .spec()
+                    .post('/auth/signup')
+                    .withBody({
+                        password: requestBody.email,
+                    })
+                    .expectStatus(expectedStatus);
+            });
+            it('Signup is not successful and an error is thrown (empty body) ', () => {
+                const expectedStatus = 400;
+                return pactum
+                    .spec()
+                    .post('/auth/signup')
+                    .withBody({})
+                    .expectStatus(expectedStatus);
+            });
+        });
+        describe('Signin', () => {
+            it('should signin successfully', () => {
+                return pactum
+                    .spec()
+                    .post('/auth/signin')
+                    .withBody(requestBody)
+                    .expectStatus(200)
+                    .stores('userAt', 'access_token');
+            });
+        });
+    });
 
- describe('User test', () => {
-  describe('Get current user (me)', () => {
-   it('should get current user', () => {
-    return pactum
-     .spec()
-     .get('/users/me')
-     .withHeaders({ Authorization: 'Bearer $S{userAt}' })
-     .expectStatus(200);
-   });
-  });
+    describe('User test', () => {
+        describe('Get current user (me)', () => {
+            it('should get current user', () => {
+                return pactum
+                    .spec()
+                    .get('/users/me')
+                    .withHeaders({
+                        Authorization: 'Bearer $S{userAt}',
+                    })
+                    .expectStatus(200);
+            });
+        });
 
-  describe('Edit user', () => {});
- });
+        describe('Edit user', () => {
+            it('should edit current user', () => {
+                const dto: EditUserDto = {
+                    email: 'test@test.com',
+                    lastName: 'test',
+                };
+                return pactum
+                    .spec()
+                    .patch('/users')
+                    .withHeaders({
+                        Authorization: 'Bearer $S{userAt}',
+                    })
+                    .withBody(dto)
+                    .expectStatus(200);
+            });
+        });
+    });
 
- describe('Bookmark test', () => {
-  describe('Create bookmark', () => {});
-  describe('Get bookmarks', () => {});
-  describe('Get bookmark by id', () => {});
-  describe('Edit bookmark', () => {});
-  describe('Delete bookmark', () => {});
- });
+    describe('Bookmark test', () => {
+        describe('Create bookmark by id', () => {});
+        describe('Get bookmarks by id', () => {});
+        describe('Get bookmark by id', () => {});
+        describe('Edit bookmark by id', () => {});
+        describe('Delete bookmark by id', () => {});
+    });
 
- afterAll(async () => {
-  app.close();
- });
+    afterAll(async () => {
+        app.close();
+    });
 });
